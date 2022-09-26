d19ab5bcea254b4d173740ca83cf1ff63f885677
Jurandi Junior <jurandijr20@gmail.com>
2022-09-25 21:29:14 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/junior/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
